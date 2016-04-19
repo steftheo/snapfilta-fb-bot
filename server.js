ee7863f519dcd996022aee6d51b4b37d88f13b86
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-04-19 15:26:17 -0500

    init

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var Botkit = require('botkit')
 
-var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
-var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN;
-var port = process.env.PORT;
+var accessToken = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
+var verifyToken = process.env.FACEBOOK_VERIFY_TOKEN
+var port = process.env.PORT
 
 if (!accessToken) throw new Error('FACEBOOK_PAGE_ACCESS_TOKEN is required but missing')
 if (!verifyToken) throw new Error('FACEBOOK_VERIFY_TOKEN is required but missing')
@@ -23,7 +23,7 @@ controller.setupWebserver(port, function (err, webserver) {
 })
 
 controller.hears(['hello', 'hi'], 'message_recieved', function (bot, message) {
-  bot.reply(message, 'Hello! Welcome to SnapFilta!')
+  bot.reply(message, 'Hello! Welcome to SnapDesigns!')
   bot.reply(message, 'I just have to ask a couple of questions before we get started')
   bot.reply(message, 'First, will this be for Personal or Business use?')
   bot.reply(message, {
